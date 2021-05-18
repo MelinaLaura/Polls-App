1756744bb6970a378a9f3d8e092dba35dde3a7d5
MelinaLaura <83724903+MelinaLaura@users.noreply.github.com>
2021-05-18 15:21:21 +0200

    added active field to Question

--- polls/admin.py
+++ polls/admin.py
@@ -14,6 +14,7 @@ class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+        (None,               {'fields': ['active']}),
     ]
     inlines = [ChoiceInline]
     list_display = ('question_text', 'pub_date', 'was_published_recently')
